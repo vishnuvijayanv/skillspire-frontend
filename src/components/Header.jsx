f2009738499a4072bd2f031d52d7356cbc4e4a72
vishnuvijayanv <vishnuvijayan0992@gmail.com>
2024-01-31 11:22:33 +0530

    logo updated

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,7 +2,7 @@ import React, { useState ,useEffect} from 'react'
 import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
-import Logo from '../Images/Blue Minimalist Letter D Logo.png'
+import Logo from '../Images/Blue_Minimalist_Letter_D_Logo-removebg-preview.png'
 
 
 function Header() {
